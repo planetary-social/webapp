cd9e9dd7531734e41acc2141d1349da35aba7f65
nichoth <nichoth@gmail.com>
2021-11-10 12:50:54 -0800

    functions

--- api/test.js
+++ api/test.js
@@ -1,9 +1,18 @@
-export default function handler (req, res) {
-  res.status(200).json({
-    body: req.body,
-    env: { foo: process.env.FOO },
-    query: req.query,
-    cookies: req.cookies,
-  });
+// ntl version
+exports.handler = function (ev, ctx, cb) {
+    return cb(null, {
+        statusCode: 200,
+        body: 'hello'
+    })
 }
+
+// vercel version
+// export default function handler (req, res) {
+//   res.status(200).json({
+//     body: req.body,
+//     env: { foo: process.env.FOO },
+//     query: req.query,
+//     cookies: req.cookies,
+//   });
+// }
 
