d9f198b2c5850701d98e9c9dd74784463e4c7f5e
nichoth <nichoth@gmail.com>
2021-11-10 11:02:35 -0800

    fix fn

--- api/test.js
+++ api/test.js
@@ -1,7 +1,7 @@
 export default function handler (req, res) {
   res.status(200).json({
     body: req.body,
-    env: { foo: process.env.FOO }
+    env: { foo: process.env.FOO },
     query: req.query,
     cookies: req.cookies,
   });
