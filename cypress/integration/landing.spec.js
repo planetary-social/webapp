1db1ed537545946f4859d09590fdf11b5cd128ac
nichoth <nichoth@gmail.com>
2021-11-15 10:46:14 -0800

    js renders

--- cypress/integration/landing.spec.js
+++ cypress/integration/landing.spec.js
@@ -3,6 +3,7 @@ var URL = 'http://localhost:8888'
 describe('the first page you see, without an account', () => {
     it('should show some text about making an account', () => {
         cy.visit(URL)
+        // @TODO
         cy.get('#content')
             .contains('hello')
             .should('be.visible')
